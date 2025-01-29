e59420cb4e73c35f6fbd11ac950a07ebc1a13db7
Nilay45 <nilay.15samanta@gmail.com>
2025-01-29 23:37:13 +0530

    profile page change

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,16 +1,21 @@
 import React, { useContext } from "react";
 import { Context } from "../main";
 import Loader from "../components/Loader";
+import "../styles/profile.css"; // Import the CSS file
 
 const Profile = () => {
   const { isAuthenticated, loading, user } = useContext(Context);
 
-  return loading ? (
-    <Loader />
-  ) : (
-    <div>
-      <h1>{user?.name}</h1>
-      <p>{user?.email}</p>
+  if (loading) {
+    return <Loader />;
+  }
+
+  return (
+    <div className="profile-container">
+      <div className="profile-card">
+        <h1 className="profile-name">{user?.name}</h1>
+        <p className="profile-email">{user?.email}</p>
+      </div>
     </div>
   );
 };
